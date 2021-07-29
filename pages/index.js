8e665512549b22e5efd1f0333a6af2d10f5f8daf
captnseagraves <k.s.seagraves@gmail.com>
2021-07-29 14:45:54 -0600

    implemented simple react hooks for input state

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,18 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Image from 'next/image'
+import React, { useState, useEffect } from 'react';
+
 
 
 export default function Home() {
+  const [addresses, setAddresses] = useState({
+    ethAddress: '',
+    tokenAddress: ''
+  })
+
+  useEffect(() => console.log(addresses), [addresses]);
+
   return (
     <Layout home>
       <Head>
@@ -12,11 +21,28 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Enter an Ethereum address and a token address to find the balance for that address</p>
-        <input type="text" name="name" placeholder="Ethereum address" />
+        <input 
+          type="text"  
+          name="ethAddressInput" 
+          placeholder="Ethereum address" 
+            onChange={event => setAddresses({
+              ethAddress: event.target.value,
+              tokenAddress: addresses.tokenAddress
+            })}/>
         <br />
-        <input type="text" name="name" placeholder="Token address" />
+        <input 
+          type="text" 
+          name="tokenAddressInput" 
+          placeholder="Token address"
+          onChange={event => setAddresses({
+            ethAddress: addresses.ethAddress,
+            tokenAddress: event.target.value
+          })}/>
         <br />
-        <input type="submit" value="Find balance" />
+        <input 
+          type="submit" 
+          value="Find balance" 
+          />
       </section>
       <section>
         <Image
