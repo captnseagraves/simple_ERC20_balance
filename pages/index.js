d95806685c1e1d34ad9dc52940b9a27068c92aaf
captnseagraves <k.s.seagraves@gmail.com>
2021-07-29 17:53:39 -0600

    implemented ethers.js provider. in progress on fethcing contract ABI from etherscan. working out how to properly make external calls using hooks on click.

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,57 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Image from 'next/image'
 import React, { useState, useEffect } from 'react';
+import { ethers } from "ethers";
+import useSWR from 'swr'
 
+const fetcher = url => fetch(url).then(r => r.json())
 
+// const getServerSideProps = async (context) => {
+//   "if ethAddress and tokenAddress are valid"
+//   "Make etherscan API call"
+//   "If successfull return contract object"
+//   "else return 'null'"
+//   return {
+//     props: {
+
+//     }
+//   }
+// }
 
 export default function Home() {
   const [ethAddress, setEthAddress] = useState('')
   const [tokenAddress, setTokenAddress] = useState('')
+  const [provider, setProvider] = useState({})
+  const [contract, setContract] = useState({})
 
-  useEffect(() => console.log("ethAddress", ethAddress, "tokenAddress", tokenAddress));
+  const handleClick = () => {
+    const { data, error } = useSWR(`https://api.etherscan.io/api?module=contract&action=getabi&address=${tokenAddress}&apikey=${process.env.ETHERSCANAPIKKEY}`, fetcher)
+
+    console.log('data', data)
+    console.log('error', error)
+
+
+    // if (error) return <div>failed to load</div>
+    // if (!data) return <div>loading...</div>
+
+    // return <div>hello {data}!</div>
+  }
+
+  useEffect(() => {
+       setProvider(new ethers.providers.Web3Provider(window.ethereum))
+  }, []);
+
+  console.log('ethAddress', ethAddress)
+  console.log('tokenAddress', tokenAddress)
+  console.log('provider', provider)
+  console.log('contract', contract)
+
+  // const getERC20Balance = async (ethAddress, tokenAddress) => {
+
+  //     await 
+
+  //     return (balance, tokenSymbol)
+  // }
 
   return (
     <Layout home>
@@ -34,6 +77,7 @@ export default function Home() {
         <input 
           type="submit" 
           value="Find balance" 
+          onClick={handleClick}
           />
       </section>
       <section>
